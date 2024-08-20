a5b99098431072ca3e22b0e55392059c991c0282
xwings <kj@xandora.net>
2024-08-20 22:02:18 +0800

    update makefile

---
+++ scripts/signalsdrpi.mk
@@ -0,0 +1,8 @@
+# Target specific constants go here
+
+HDF_URL:=http://github.com/analogdevicesinc/plutosdr-fw/releases/download/${LATEST_TAG}/system_top.hdf
+TARGET_DTS_FILES:= zynq-signalsdrpi.dtb 
+COMPLETE_NAME:=SIGNALSDRPI
+ZIP_ARCHIVE_PREFIX:=signalsdrpi
+DEVICE_VID:=0x0456
+DEVICE_PID:=0xb673
