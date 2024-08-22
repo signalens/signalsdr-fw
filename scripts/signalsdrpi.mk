5030def1a23e1c75d554cb5cadd54110796e306b
xwings <kj@xandora.net>
2024-08-22 09:15:51 +0800

    add signalsdrpi-revb and signalsdrpi-revc

--- scripts/signalsdrpi.mk
+++ scripts/signalsdrpi.mk
@@ -1,7 +1,7 @@
 # Target specific constants go here
 
 HDF_URL:=http://github.com/analogdevicesinc/plutosdr-fw/releases/download/${LATEST_TAG}/system_top.hdf
-TARGET_DTS_FILES:= zynq-signalsdrpi.dtb 
+TARGET_DTS_FILES:= zynq-signalsdrpi.dtb zynq-signalsdrpi-revb.dtb zynq-signalsdrpi-revc.dtb
 COMPLETE_NAME:=SIGNALSDRPI
 ZIP_ARCHIVE_PREFIX:=signalsdrpi
 DEVICE_VID:=0x0456
