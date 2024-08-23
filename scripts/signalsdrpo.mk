bb9359d4e4597700b0f935d10c30eb72eb660273
xwings <kj@xandora.net>
2024-08-23 14:59:28 +0800

    standardized signalsdrpro and signalsdrpi

---
+++ scripts/signalsdrpo.mk
@@ -0,0 +1,8 @@
+# Target specific constants go here
+
+HDF_URL:=http://github.com/analogdevicesinc/plutosdr-fw/releases/download/${LATEST_TAG}/system_top.hdf
+TARGET_DTS_FILES:= zynq-signalsdrpro.dtb zynq-signalsdrpro-revb.dtb zynq-signalsdrpro-revc.dtb
+COMPLETE_NAME:=SIGNALSDRPRO
+ZIP_ARCHIVE_PREFIX:=signalsdrpro
+DEVICE_VID:=0x0456
+DEVICE_PID:=0xb673
